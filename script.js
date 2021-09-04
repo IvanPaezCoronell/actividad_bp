8b643afdf28203934169b73142772b577aef86e1
Ivan Paez Coronell <ipaez1@cuc.edu.co>
2021-09-04 11:17:02 -0500

    #3 Porcentajes de Inversion

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function pulsaciones(edad){
     return `El numero de pulsaciones es de: ${formula_pulsaciones}`;
 }
 console.log(pulsaciones (15));
+
+
+
+// #3 Porcentaje Invertido
+function porcentaje(p1,p2,p3){
+    let inversion_total = p1 + p2 + p3;
+    let porcentajep1 = (p1 / inversion_total) * 100;
+    let porcentajep2 = (p2 / inversion_total) * 100;
+    let porcentajep3 = (p3 / inversion_total) * 100;
+    return `La inversion total es de ${inversion_total}
+    \n El porcentaje invertido de la 1ra persona es de ${porcentajep1}
+    \n El porcentaje invertido de la 2da persona es de ${porcentajep2}
+    \n El porcentaje invertido de la 3ra persona es de ${porcentajep3}`;
+} 
+console.log(porcentaje(200,150,250));
